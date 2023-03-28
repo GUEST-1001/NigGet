0ca42a21372e59289e6e6752ad4514d6eac19510
GUEST-1001 <nawatgame@live.com>
2023-03-29 02:58:09 +0700

    save point

---
+++ Assets/MyAsset/Script/AniConP2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class AniConP2 : NetworkBehaviour
+{
+    public Animator aniP1;
+    [SerializeField] bool isWalk = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isLocalPlayer)
+        {
+            AniSet();
+            ChackIsWalk();
+        }
+    }
+    void ChackIsWalk()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > Mathf.Epsilon || Mathf.Abs(Input.GetAxis("Vertical")) > Mathf.Epsilon)
+        {
+            isWalk = true;
+        }
+        else
+        {
+            isWalk = false;
+        }
+    }
+
+    void AniSet()
+    {
+        aniP1.SetBool("Run", isWalk);
+    }
+}
